524e02f97ebc2aa7f229185a3c9aa486d463cf97
Abdullah Al NOMAN <abdullahalnomancse@gmail.com>
2022-03-30 09:43:09 +0530

    all edited

--- js-1/javaScriptfile.js
+++ js-1/javaScriptfile.js
@@ -1,35 +1,22 @@
-// let a = 10;
-// console.log(a);
-// let b = [1, 2, 3, 4, 5];
-// console.log(b.length);
-// console.log("hello Noman");
-// var b = 100;
+// var intervalID;
+// var runcout = 0;
 
-// function fun() {
-//     c = 20;
-//     let a = 5;
-
-//     if (a == 5) {
-//         var b = 10;
-//         console.log("Inside b ", b);
-//     }
-//     b = 30;
-//     console.log("Outside b", b);
+// function syaHEllo() {
+//   runcout++;
+//   if (runcout > 5) {
+//     clearInterval(intervalID);
+//   }
+//   console.log("Hello!!");
 // }
-// console.log(b);
+// console.log("And the wait starts............");
+// intervalID = setInterval(syaHEllo, 1000);
 
 
-// function squerRT(n) {
-//     return Math.sqrt(n);
+// setInterval(() => {
+//     console.log("This i is aftyer 1 secend");
+// }, 1000);
+
+// var myfub = function () {
+//     console.log("ths is 1 secend");
 // }
-
-var squre_root = function(param) {
-    return Math.sqrt(param);
-}
-console.log(squre_root(5));
-
-
-const StringNmae = function(noman) {
-    console.log("Abdullah Al Noan");
-}
-var Name = StringNmae();
+// setInterval(myfub,10000)
